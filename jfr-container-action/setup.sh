0be559a1428338ba54e7cc1de5555b7ca67fd1b1
Yiming Gong <277645526@qq.com>
2022-06-17 10:26:02 -0400

    fix path error

--- jfr-container-action/setup.sh
+++ jfr-container-action/setup.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
+WORK_DIR=/github/workflow
+
 echo "Set up Jenkins war package."
 cd /app/jenkins && jar -cvf jenkins.war *
 
 echo "Download plugins."
-java -jar /app/bin/jenkins-plugin-manager.jar --war /app/jenkins/jenkins.war --plugin-file ${GITHUB_WORKSPACE}/"$3" --plugin-download-directory=/usr/share/jenkins/ref/plugins
+java -jar /app/bin/jenkins-plugin-manager.jar --war /app/jenkins/jenkins.war --plugin-file ${WORK_DIR}/"$3" --plugin-download-directory=/usr/share/jenkins/ref/plugins
 
 if [ $# == 4 ]
 then
     echo "Set up JCasC."
-    cp ${GITHUB_WORKSPACE}/"$4" ${CASC_JENKINS_CONFIG}
+    cp ${WORK_DIR}/"$4" ${CASC_JENKINS_CONFIG}
 fi
 
 echo "Running Jenkins pipeline."
-/app/bin/jenkinsfile-runner-launcher ${GITHUB_WORKSPACE}/"$1" -w /app/jenkins -p /usr/share/jenkins/ref/plugins -f ${GITHUB_WORKSPACE}/"$2"
+/app/bin/jenkinsfile-runner-launcher ${WORK_DIR}/"$1" -w /app/jenkins -p /usr/share/jenkins/ref/plugins -f ${WORK_DIR}/"$2"
