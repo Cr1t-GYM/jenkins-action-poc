9f81b14f5739a2705c208260f146cb84030b1459
Yiming Gong <277645526@qq.com>
2022-06-22 11:31:34 -0400

    fix env error

--- jfr-container-action/download.sh
+++ jfr-container-action/download.sh
@@ -4,9 +4,9 @@ set -e
 if [ $# == 1 ]
 then 
     echo "Download plugins."
-    java -jar /app/bin/jenkins-plugin-manager.jar --war /app/jenkins/jenkins.war --plugin-file "$1" --plugin-download-directory=${GITHUB_WORKSPACE}/jenkins_new_plugins
-    ls -al ${GITHUB_WORKSPACE}/jenkins_new_plugins
-    cp -r ${GITHUB_WORKSPACE}/jenkins_new_plugins/* /usr/share/jenkins/ref/plugins
+    java -jar /app/bin/jenkins-plugin-manager.jar --war /app/jenkins/jenkins.war --plugin-file "$1" --plugin-download-directory=/jenkins_new_plugins
+    ls -al /jenkins_new_plugins
+    cp -r /jenkins_new_plugins/* /usr/share/jenkins/ref/plugins
 else
     echo "No plugins downloaded."
 fi
