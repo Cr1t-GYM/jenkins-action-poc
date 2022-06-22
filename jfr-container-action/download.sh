e76205dd9e28d361eb472416053f8894cc8fd7ae
Yiming Gong <277645526@qq.com>
2022-06-22 11:09:59 -0400

    add new plugins info

--- jfr-container-action/download.sh
+++ jfr-container-action/download.sh
@@ -6,6 +6,7 @@ if [ $# == 1 ]
 then 
     echo "Download plugins."
     java -jar /app/bin/jenkins-plugin-manager.jar --war /app/jenkins/jenkins.war --plugin-file "$1" --plugin-download-directory=${GITHUB_WORKSPACE}/jenkins_new_plugins
+    ls -al ${GITHUB_WORKSPACE}/jenkins_new_plugins
     cp -r ${GITHUB_WORKSPACE}/jenkins_new_plugins/* /usr/share/jenkins/ref/plugins
 else
     echo "No plugins downloaded."
