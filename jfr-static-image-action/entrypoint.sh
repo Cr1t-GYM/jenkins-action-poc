35fcb776287da5eee574165abfe0ae1722a88f94
Yiming Gong <277645526@qq.com>
2022-07-06 12:48:05 -0400

    add debug log

--- jfr-static-image-action/entrypoint.sh
+++ jfr-static-image-action/entrypoint.sh
@@ -2,7 +2,9 @@
 set -e
 
 echo "Download plugins."
+ls -al /usr/share/jenkins/ref/plugins
 java -jar /app/bin/jenkins-plugin-manager.jar --war /app/jenkins/jenkins.war --plugin-file "$3" --plugin-download-directory=/usr/share/jenkins/ref/plugins
+ls -al /usr/share/jenkins/ref/plugins
 
 if [[ $# == 4 && $4 != "" ]]
 then
