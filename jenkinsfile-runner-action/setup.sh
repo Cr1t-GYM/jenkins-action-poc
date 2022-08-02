ba84f9892021407bcfa3eb8a4b506694063b23f6
Yiming Gong <277645526@qq.com>
2022-08-02 17:53:34 -0400

    refactor jfr action

--- jenkinsfile-runner-action/setup.sh
+++ jenkinsfile-runner-action/setup.sh
@@ -2,13 +2,11 @@
 set -e
 
 JENKINS_ROOT=./jenkins
-JFR_VERSION=1.0-beta-30
 
 if [ $# == 3 ]
 then
-    cp ${GITHUB_WORKSPACE}/$3 ${GITHUB_WORKSPACE}/jenkins/casc
+    cp $3 ${GITHUB_WORKSPACE}/jenkins/casc
 fi
 
-echo "Executing the pipeline"
-echo "${JENKINS_ROOT}/jenkinsfile-runner-${JFR_VERSION}/bin/jenkinsfile-runner "$1" -w ${JENKINS_ROOT}/jenkins -p ${JENKINS_ROOT}/plugins -f $2"
-${JENKINS_ROOT}/jenkinsfile-runner-${JFR_VERSION}/bin/jenkinsfile-runner "$1" -w ${JENKINS_ROOT}/jenkins -p ${JENKINS_ROOT}/plugins -f $2
+echo "Executing the pipeline..."
+${JENKINS_ROOT}/jenkinsfile-runner/bin/jenkinsfile-runner "$1" -w ${JENKINS_ROOT}/jenkins -p ${JENKINS_ROOT}/plugins -f $2
