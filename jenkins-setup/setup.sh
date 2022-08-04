157b366b1357af1136cc1784b7fc985842c20179
Yiming Gong <277645526@qq.com>
2022-08-04 10:25:54 -0400

    fix syntax

--- jenkins-setup/setup.sh
+++ jenkins-setup/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -exuo pipefail
 
-echo "{JENKINS_ROOT}={$JENKINS_ROOT}" >> $GITHUB_ENV
+echo "{JENKINS_ROOT}={$JENKINS_ROOT}" >> "$GITHUB_ENV"
 # JENKINS_ROOT=./jenkins
 # JENKINS_VERSION=2.319.3
 # JENKINS_PM_VERSION=2.5.0
@@ -17,22 +17,22 @@ echo "{JENKINS_ROOT}={$JENKINS_ROOT}" >> $GITHUB_ENV
 # JENKINS_JFR_URL="${JENKINS_JFR_URL:-https://github.com/jenkinsci/jenkinsfile-runner/releases/download/${JFR_VERSION}/jenkinsfile-runner-${JFR_VERSION}.zip}"
 
 # download Jenkins core
-mkdir -p ${JENKINS_ROOT}
+mkdir -p "${JENKINS_ROOT}"
 echo "Downloading Jenkins $JENKINS_VERSION core..."
-curl -s -L "${JENKINS_CORE_URL}" -o ${JENKINS_ROOT}/jenkins.war
+curl -s -L "${JENKINS_CORE_URL}" -o "${JENKINS_ROOT}"/jenkins.war
 # download Jenkinsfile-runner
 echo "Downloading Jenkinsfile runner..."
-curl -s -L "${JENKINS_JFR_URL}" -o ${JENKINS_ROOT}/jenkinsfile-runner-${JFR_VERSION}.zip
-unzip -q ${JENKINS_ROOT}/jenkinsfile-runner-${JFR_VERSION}.zip -d ${JENKINS_ROOT}/jenkinsfile-runner
-chmod +x ${JENKINS_ROOT}/jenkinsfile-runner/bin/jenkinsfile-runner
+curl -s -L "${JENKINS_JFR_URL}" -o "${JENKINS_ROOT}"/jenkinsfile-runner-"${JFR_VERSION}".zip
+unzip -q "${JENKINS_ROOT}"/jenkinsfile-runner-"${JFR_VERSION}".zip -d "${JENKINS_ROOT}"/jenkinsfile-runner
+chmod +x "${JENKINS_ROOT}"/jenkinsfile-runner/bin/jenkinsfile-runner
 mkdir -p "${GITHUB_WORKSPACE}/jenkins/casc"
-unzip -q ${JENKINS_ROOT}/jenkins.war -d ${JENKINS_ROOT}/jenkins
-mkdir -p ${JENKINS_ROOT}/jenkins/WEB-INF/groovy.init.d
-cp "${GITHUB_ACTION_PATH}/init.groovy" ${JENKINS_ROOT}/jenkins/WEB-INF/groovy.init.d/
+unzip -q "${JENKINS_ROOT}"/jenkins.war -d "${JENKINS_ROOT}"/jenkins
+mkdir -p "${JENKINS_ROOT}"/jenkins/WEB-INF/groovy.init.d
+cp "${GITHUB_ACTION_PATH}/init.groovy" "${JENKINS_ROOT}"/jenkins/WEB-INF/groovy.init.d/
 # download plugin manager
 echo "Downloading Jenkins plugin manager..."
-curl -s -L "$JENKINS_PM_URL" -o ${JENKINS_ROOT}/jenkins-plugin-manager.jar
+curl -s -L "$JENKINS_PM_URL" -o "${JENKINS_ROOT}"/jenkins-plugin-manager.jar
 echo "Downloading latest minimum required plugins for pipeline..."
-java -jar ${JENKINS_ROOT}/jenkins-plugin-manager.jar --war ${JENKINS_ROOT}/jenkins.war --plugin-file "${GITHUB_ACTION_PATH}/plugins.txt" --plugin-download-directory=${JENKINS_ROOT}/plugins
+java -jar "${JENKINS_ROOT}"/jenkins-plugin-manager.jar --war "${JENKINS_ROOT}"/jenkins.war --plugin-file "${GITHUB_ACTION_PATH}/plugins.txt" --plugin-download-directory="${JENKINS_ROOT}"/plugins
 
 echo "Jenkins environment has been set up..."
