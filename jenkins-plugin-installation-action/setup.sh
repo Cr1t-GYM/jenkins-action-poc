489417e2332b9e193c8789a4f1f380c5399fc0e9
Yiming Gong <277645526@qq.com>
2022-08-04 12:26:47 -0400

    use long opts

--- jenkins-plugin-installation-action/setup.sh
+++ jenkins-plugin-installation-action/setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -exo pipefail
+set -euo pipefail
 
 PLUGIN_LIST=plugins.txt
 
--- jenkins-setup/setup.sh
+++ jenkins-setup/setup.sh
@@ -1,16 +1,7 @@
 #!/usr/bin/env bash
-set -exuo pipefail
+set -euo pipefail
 
 echo "JENKINS_ROOT=$JENKINS_ROOT" >> "$GITHUB_ENV"
-# JENKINS_ROOT=./jenkins
-# JENKINS_VERSION=2.319.3
-# JENKINS_PM_VERSION=2.5.0
-# JFR_VERSION=1.0-beta-30
-
-# if [ $# == 1 ]
-# then
-#     JENKINS_VERSION=$1
-# fi
 if [[ "$JENKINS_PM_URL" =~ ^https://github.com/jenkinsci/plugin-installation-manager-tool.* ]]
 then
     JENKINS_PM_URL=https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/"${JENKINS_PM_VERSION}"/jenkins-plugin-manager-"${JENKINS_PM_VERSION}".jar
@@ -24,18 +15,13 @@ then
     JENKINS_JFR_URL=https://github.com/jenkinsci/jenkinsfile-runner/releases/download/"${JFR_VERSION}"/jenkinsfile-runner-"${JFR_VERSION}".zip
 fi
 
-
-# JENKINS_PM_URL="${JENKINS_PM_URL:-https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/${JENKINS_PM_VERSION}/jenkins-plugin-manager-${JENKINS_PM_VERSION}.jar}"
-# JENKINS_CORE_URL="${JENKINS_CORE_URL:-https://updates.jenkins.io/download/war/${JENKINS_VERSION}/jenkins.war}"
-# JENKINS_JFR_URL="${JENKINS_JFR_URL:-https://github.com/jenkinsci/jenkinsfile-runner/releases/download/${JFR_VERSION}/jenkinsfile-runner-${JFR_VERSION}.zip}"
-
 # download Jenkins core
 mkdir -p "${JENKINS_ROOT}"
 echo "Downloading Jenkins $JENKINS_VERSION core..."
-curl -s -L "${JENKINS_CORE_URL}" -o "${JENKINS_ROOT}"/jenkins.war
+curl --silent -L "${JENKINS_CORE_URL}" -o "${JENKINS_ROOT}"/jenkins.war
 # download Jenkinsfile-runner
 echo "Downloading Jenkinsfile runner..."
-curl -s -L "${JENKINS_JFR_URL}" -o "${JENKINS_ROOT}"/jenkinsfile-runner-"${JFR_VERSION}".zip
+curl --silent -L "${JENKINS_JFR_URL}" -o "${JENKINS_ROOT}"/jenkinsfile-runner-"${JFR_VERSION}".zip
 unzip -q "${JENKINS_ROOT}"/jenkinsfile-runner-"${JFR_VERSION}".zip -d "${JENKINS_ROOT}"/jenkinsfile-runner
 chmod +x "${JENKINS_ROOT}"/jenkinsfile-runner/bin/jenkinsfile-runner
 mkdir -p "${GITHUB_WORKSPACE}/jenkins/casc"
@@ -44,7 +30,7 @@ mkdir -p "${JENKINS_ROOT}"/jenkins/WEB-INF/groovy.init.d
 cp "${GITHUB_ACTION_PATH}/init.groovy" "${JENKINS_ROOT}"/jenkins/WEB-INF/groovy.init.d/
 # download plugin manager
 echo "Downloading Jenkins plugin manager..."
-curl -s -L "$JENKINS_PM_URL" -o "${JENKINS_ROOT}"/jenkins-plugin-manager.jar
+curl --silent -L "$JENKINS_PM_URL" -o "${JENKINS_ROOT}"/jenkins-plugin-manager.jar
 echo "Downloading latest minimum required plugins for pipeline..."
 java -jar "${JENKINS_ROOT}"/jenkins-plugin-manager.jar --war "${JENKINS_ROOT}"/jenkins.war --plugin-file "${GITHUB_ACTION_PATH}/plugins.txt" --plugin-download-directory="${JENKINS_ROOT}"/plugins
 
